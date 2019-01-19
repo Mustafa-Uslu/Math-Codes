9adc82d547aa2815508c3124d26b76af9f6928b7
SULEYMAN ALPTEKIN <slptkn@yahoo.com>
2019-01-19 16:41:20 -0500

    Try to commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,11 +3,11 @@ public class Calculator {
         return num1+num2;
     }
 
-    public static double subtract(double num1, double num2){
-        double result = num1-num2;
+    public static double subtract(double numA, double numB){
+        double result = numA-numB;
         return result;
     }
     public static double multiply(double num1, double num2){
-         return = num1*num2;
+         return num1*num2;
     }
 }
